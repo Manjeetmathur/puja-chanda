511acadcf7a2cc0432b544add65e7631fa95ddf8
Manjeetmathur <manjeetkumar62054@gmail.com>
2025-03-19 00:45:20 +0530

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
        {
-              origin:"http://localhost:5173",
+              origin:["https://puja-chanda.vercel.app","http://localhost:5173"],
               credentials:true
        }
 ));
