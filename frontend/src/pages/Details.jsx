5ea8f826a15febe8f965fa5cdb2846dc0836207a
Manjeetmathur <manjeetkumar62054@gmail.com>
2025-03-21 21:35:04 +0530

    first commit

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -50,13 +50,13 @@ const Details = () => {
                                                         key={user?._id}
                                                         className={`flex justify-between items-center py-3 border-b border-gray-100 hover:bg-orange-50 transition-all duration-200`} 
                                                  >
-                                                        <span className={`${user.pujaChanda < 275 && user.pujaChanda > 0 &&  'text-yellow-500' } ${user.pujaChanda === 0 && 'text-red-700' }  text-md text-blue-500 w-[120px]`}>
+                                                        <span className={` ${user.pujaChanda === 0 && 'text-red-700' }   text-md text-blue-500 w-[120px]`}>
                                                                {index + 1}. {user.name}
                                                         </span>
-                                                        <span className={ `${user.pujaChanda === 0 && 'text-red-700' } text-md text-green-700 text-end`}>
+                                                        <span className={ ` ${user.pujaChanda === 0 && 'text-red-700' }   text-md text-blue-500 `}>
                                                                 {user.pujaChanda}
                                                         </span>
-                                                        <span className={`text-md text-green-700 ${user.khanaChanda === 0 && 'text-red-700'}`}>
+                                                        <span className={` ${user.pujaChanda === 0 && 'text-red-700' }   text-md text-blue-500 `}>
                                                               {user.khanaChanda}
                                                         </span>
                                                        
